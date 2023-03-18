509e9debd02eafee7978638f0ab442bc0fa895d7
bita.karvizi <bita.karvizi1379@gmail.com>
2023-03-18 13:07:38 +0330

    Implement Splash Fragment(#4)
    * Implement fragment_splash.xml and SplashFragment.kt
    * Implement main activity and navigation
    * Handle showing toolbar in main activity

---
+++ app/src/main/java/com/application/presence_absence/ui/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.application.presence_absence.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.application.presence_absence.R
+import com.application.presence_absence.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    private val topLevelMenuItems = setOf<Int>(
+        //TODO: add top level fragments
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupNavController()
+        setupNavigationUiState()
+    }
+
+    private fun setupNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+    }
+
+    private fun setupNavigationUiState() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            with(binding) {
+                when (destination.id) {
+                    // Hide UI controllers
+                    in topLevelMenuItems -> {
+                        toolbar.visibility = View.VISIBLE
+                    }
+
+                    else -> {
+                        toolbar.visibility = View.GONE
+                    }
+                }
+            }
+        }
+    }
+}
