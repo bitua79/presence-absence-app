31b49eb856cf682195a03a677a75ee9e2a47b422
bita.karvizi <bita.karvizi1379@gmail.com>
2023-03-18 17:00:59 +0330

    Implement Login Fragment(#7)
    * Implement fragment_login.xml and LoginFragment.kt
    * Add login strings
    * Implement navigation to login
    * Add LoginFragment as top level menu in MainActivity.kt
    * Rename logo_png to ic_logo.png

---
+++ app/src/main/java/com/application/presence_absence/ui/LoginFragment.kt
@@ -0,0 +1,50 @@
+package com.application.presence_absence.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.application.presence_absence.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+    }
+
+    private fun initViews() {
+
+
+        // button listener
+        binding.btnLogin.setOnClickListener {
+            goExamList()
+        }
+    }
+
+    private fun checkInput() {
+        with(binding) {
+
+            val username = etUsername.text?.trim() ?: ""
+            val pass = etPassword.text?.trim() ?: ""
+
+            // TODO: send username/pass to server and verify user
+        }
+    }
+
+    private fun goExamList() {
+        //TODO: implement navigation to exam list page
+    }
+}
