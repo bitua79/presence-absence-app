44595731ec746f110ebd72fa3d2958059ef1fb34
bita.karvizi <bita.karvizi1379@gmail.com>
2023-12-06 12:32:54 +0330

    Update gradle to kts (#31)
    * Rename .gradle files to DSL and refactor codes

---
+++ app/build.gradle.kts
@@ -0,0 +1,83 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+
+    // Parcelize
+    id("org.jetbrains.kotlin.plugin.parcelize")
+
+    // Hilt
+    id("dagger.hilt.android.plugin")
+
+    // Navigation
+    id("androidx.navigation.safeargs.kotlin")
+}
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.application.presence_absence"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+
+    // UI
+    implementation("androidx.activity:activity-ktx:1.6.1")
+    implementation("com.google.android.material:material:1.6.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.airbnb.android:lottie:5.2.0")
+
+    // Glide
+    kapt("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.github.2coffees1team:GlideToVectorYou:v2.0.0")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
+    // Hilt
+    kapt("com.google.dagger:hilt-android-compiler:2.38.1")
+    implementation("com.google.dagger:hilt-android:2.36")
+    implementation("androidx.hilt:hilt-work:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Gradle
+        classpath("com.android.tools.build:gradle:7.0.4")
+
+        // Kotlin
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+
+        // Hilt
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
+
+        // Navigation
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
+    }
+}
+allprojects {
+    repositories {
+        google()
+        maven(url = "https://jitpack.io")
+        mavenCentral()
+    }
+}
+
+tasks.register("clean").configure {
+    delete("build")
+}
