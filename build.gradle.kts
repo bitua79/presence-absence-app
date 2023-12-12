7a9d091ff2d29dc329a3676350a596dc155a8fd1
bita <bita.karvizi1379@gmail.com>
2023-12-13 02:12:38 +0330

    Update Dependencies(#35)
    * Replace repositories by personal maven in settings.gradle.kts and build.gradle.kts
    * Update SDK version
    * Update kotlin and KSP version
    * Update gradle to 8 and replace applicationId with namespace
    * Update dependencies version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,37 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
-        google()
-        mavenCentral()
+        maven {
+            url = uri("https://inexus.samentic.com/repository/samentic-android/")
+            credentials {
+                username = "bita.karvizi"
+                password = "5'&RER>>v^Xsv7?"
+            }
+        }
     }
     dependencies {
         // Gradle
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:8.0.2")
 
         // Kotlin
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
 
         // Hilt
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
 
         // Navigation
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
     }
 }
 allprojects {
     repositories {
-        google()
-        maven(url = "https://jitpack.io")
-        mavenCentral()
+        maven {
+            url = uri("https://inexus.samentic.com/repository/samentic-android/")
+            credentials {
+                username = "bita.karvizi"
+                password = "5'&RER>>v^Xsv7?"
+            }
+        }
     }
 }
 
