a789f8f1d75a577d7f5d734bff25bdeb0d4ba166
bita <bita.karvizi1379@gmail.com>
2024-01-05 15:44:05 +0330

    Implement Rootbeer in project (#80)
    * Fix Splash duration
    * Fix splash package
    * Add Rootbeer dependancy
    * Implement root detection and emulator detection in DetectionUtils
    * Check rootedness in SplashFragment
    * Implement a function in ViewExtensions.kt to show snackbar

--- app/src/main/java/com/application/presence_absence/ui/utils/ViewExtensions.kt
+++ app/src/main/java/com/application/presence_absence/ui/utils/ViewExtensions.kt
@@ -3,10 +3,16 @@ package com.application.presence_absence.ui.utils
 import android.app.Activity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.application.presence_absence.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.snackbar.Snackbar
 import saman.zamani.persiandate.PersianDate
+import kotlin.system.exitProcess
 
 
 fun View.gone() {
@@ -45,6 +51,26 @@ fun BottomSheetDialog.createDialog(): BottomSheetDialog {
     return this
 }
 
+fun Fragment.createSnackbar(@StringRes message: Int, @StringRes actionText: Int): Snackbar {
+    val snackbar = Snackbar.make(
+        requireView(),
+        getString(message),
+        Snackbar.LENGTH_INDEFINITE
+    )
+
+    with(snackbar) {
+        setAction(getString(actionText)) {
+            exitProcess(1)
+        }
+        setBackgroundTint(getColor(R.color.color_surface))
+        setTextColor(getColor(R.color.color_on_surface))
+
+        setActionTextColor(getColor(R.color.color_primary))
+        animationMode = Snackbar.ANIMATION_MODE_FADE
+    }
+
+    return snackbar
+}
 
 fun Fragment.hideKeyboard() {
     val imm =
@@ -58,4 +84,8 @@ fun Fragment.hideKeyboard() {
 
 fun PersianDate.getShDate(): String {
     return "${dayName()} $shMonth/$shDay"
+}
+
+fun Fragment.getColor(@ColorRes colorId: Int): Int {
+    return ContextCompat.getColor(requireContext(), colorId)
 }
